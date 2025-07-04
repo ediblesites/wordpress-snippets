72134a99e6e5a456899f6fe1fc69ef6b6ae18ad4
adam-marash <35106583+adam-marash@users.noreply.github.com>
2025-07-04 19:25:16 +0100

    Update bbb.php

--- bbb.php
+++ bbb.php
@@ -1,1 +1,14 @@
-slijfalj
+<?php
+/**
+ * Snippet: Custom ShmoooCommerce checkout fields
+ * Description: Adds company VAT field to checkout with validation
+ * Tags: all
+ * Required: 
+ * Secrets: 
+ * Version: 1.2.0
+ * Author: Adam
+ * Priority: 10
+ * Context: frontend, backend
+ */
+
+echo "slijfalj";
