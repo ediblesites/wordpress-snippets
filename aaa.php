c46e994ab55dfcc8c99d844a57db613cf9fdb510
adam-marash <35106583+adam-marash@users.noreply.github.com>
2025-07-04 19:24:06 +0100

    Update aaa.php

--- aaa.php
+++ aaa.php
@@ -1,1 +1,14 @@
-asdja;dlsjf
+<?php
+/**
+ * Snippet: Custom WooCommerce checkout fields
+ * Description: Adds company VAT field to checkout with validation
+ * Tags: woocommerce, checkout, ecommerce
+ * Required: woocommerce>=5.0
+ * Secrets: vat_api_key
+ * Version: 1.2.0
+ * Author: Your Name
+ * Priority: 10
+ * Context: frontend
+ */
+
+echo "asdja;dlsjf";
